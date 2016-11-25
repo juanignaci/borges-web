0102fd3d9b88eaabeb01603b6fa7e435108e2ee0
juanignaci <juanignaci3@gmail.com>
2016-11-25 17:48:40 -0300

    Summary
    Description

--- public/js/Cluster.js
+++ public/js/Cluster.js
@@ -1,36 +1,43 @@
 var PARTICLECOUNT = 20;
 
 Cluster.prototype = new THREE.Object3D;
-function Cluster(x, y, nodeTexture, scene, controller)
+function Cluster(x, y, scene, controller, cam)
 {
 	var particles = [];
 
 	for (var i = PARTICLECOUNT - 1; i >= 0; i--)
-	{		
+	{
 		var particle = new Node( 
-			x + (Math.random() * SCL * -2 + SCL),
-			(Math.random() * SCL * 2.5) - SCL * 0.25,
-			y + (Math.random() * SCL * -2 + SCL),
+			x + (Math.random() * sW * -2 + sW),
+			(Math.random() * sH * -2 + sH) + camera.position.y,
+			y + (Math.random() * sW * -2 + sW),
 			new THREE.SpriteMaterial( {
 			color: Math.random() * 0x808080 + 0x808080,
 			fog: true,
-			map: nodeTexture
-		} ) );
+			map: controller.materialLowDef 
+			} ), controller );
 		
 		particles[i] = particle;
+
 		scene.add( particle );
 	};
 
 	this.particleArr = particles;
 	this.controller = controller;
+	this.position = new THREE.Vector3(x * SCL, camera.position.y, y * SCL);
+	this.camera = cam;
 }
 
 Cluster.prototype.updateNodes = function()
 {
 	for (var i = this.particleArr.length - 1; i >= 0; i--) {
+		var dis = this.particleArr[i].position.distanceTo( this.camera.position );
+
 		switch(controller.state)
 		{
 			case "intro":
+				if(this.particleArr[i].asleep)
+					continue;
 				this.particleArr[i].position.set(this.particleArr[i].position.x + (this.particleArr[i].introPosition.x - this.particleArr[i].position.x) * 0.1,
 											 this.particleArr[i].position.y + ((this.particleArr[i].introPosition.y + Math.sin(Date.now()*0.001 + this.particleArr[i].position.x * 0.5 + this.particleArr[i].position.z * 0.5) * 0.3) - this.particleArr[i].position.y) * 0.1,
 											 this.particleArr[i].position.z + (this.particleArr[i].introPosition.z - this.particleArr[i].position.z) * 0.1);
@@ -39,27 +46,206 @@ Cluster.prototype.updateNodes = function()
 				this.particleArr[i].position.set(this.particleArr[i].position.x + (this.particleArr[i].nodePosition.x - this.particleArr[i].position.x) * 0.02,
 											 this.particleArr[i].position.y + ((this.particleArr[i].nodePosition.y + Math.sin(Date.now()*0.001 + this.particleArr[i].position.x) * 0.02) - this.particleArr[i].position.y) * 0.06,
 											 this.particleArr[i].position.z + (this.particleArr[i].nodePosition.z - this.particleArr[i].position.z) * 0.02);
+				
+				if(dis > 8)
+				{
+					this.particleArr[i].scale.x = this.particleArr[i].scale.y = this.particleArr[i].originalScale * 1.65;
+					this.particleArr[i].material.map = controller.materialBlurFar;
+				}					
+				else if(dis > 5)
+				{
+					this.particleArr[i].scale.x = this.particleArr[i].scale.y = this.particleArr[i].originalScale * 1.55;
+					this.particleArr[i].material.map = controller.materialBlur;
+				}					
+				else if (dis > 0.6)
+				{
+					this.particleArr[i].scale.x = this.particleArr[i].scale.y = this.particleArr[i].originalScale;
+					this.particleArr[i].material.map = controller.materialLowDef;
+				}
+				else
+				{
+					this.particleArr[i].scale.x = this.particleArr[i].scale.y = this.particleArr[i].originalScale * 1.5;
+					this.particleArr[i].material.map = controller.materialBlurNear;
+				}
+			break;
+			case "selected":
+				if(this.particleArr[i].selected)
+				{
+					this.particleArr[i].position.set(this.particleArr[i].position.x + (this.particleArr[i].targetPosition.x - this.particleArr[i].position.x) * 0.2,
+												 this.particleArr[i].position.y + (this.particleArr[i].targetPosition.y - this.particleArr[i].position.y) * 0.2,
+												 this.particleArr[i].position.z + (this.particleArr[i].targetPosition.z - this.particleArr[i].position.z) * 0.2);
+					
+					this.particleArr[i].material.map = controller.materialHiDef;
+					this.particleArr[i].scale.x = this.particleArr[i].scale.y = this.particleArr[i].originalScale;
+				} else {
+				this.particleArr[i].position.set(this.particleArr[i].position.x + (this.particleArr[i].nodePosition.x - this.particleArr[i].position.x) * 0.1,
+											this.particleArr[i].position.y + ((this.particleArr[i].introPosition.y + Math.sin(Date.now()*0.0005 + 
+											this.particleArr[i].position.x * 0.5 + this.particleArr[i].position.z * 0.5) * 0.4) - 
+											this.particleArr[i].position.y - (this.particleArr[i].introPosition.y + 3)) * 1/(dis * 5),
+											this.particleArr[i].position.z + (this.particleArr[i].nodePosition.z - this.particleArr[i].position.z) * 0.1);
+			
+					this.particleArr[i].material.map = controller.materialBlurFar;
+					this.particleArr[i].scale.x = this.particleArr[i].scale.y = this.particleArr[i].originalScale * 1.45;
+				}
 			break;
 		}
 	};
 }
 
+/*
+Cluster.prototype.moveCluster(x, y)
+{
+	for (var i = 0; i < this.particleArr.length; i++) {
+		var v3 = 
+		this.particleArr[i].position.set
+	};
+}
+*/
 function NodeController()
 {
 	this.state = "intro";
+	this.materialLowDef =  new THREE.TextureLoader().load( "images/node.png" );
+	this.materialHiDef =  new THREE.TextureLoader().load( "images/node-hidef.png" );
+	this.materialBlur =  new THREE.TextureLoader().load( "images/node-blurLess.png" );
+	this.materialBlurNear =  new THREE.TextureLoader().load( "images/node-blurNear.png" );
+	this.materialBlurFar =  new THREE.TextureLoader().load( "images/node-blur.png" );
+}
+
+function RelatedNodes(count, camPos, camForward, controller)
+{
+	console.log(camPos);
+	console.log(camForward);
+	this.particles = [];
+	this.controller = controller;
+	var v3 = new THREE.Vector3(camForward.x * 4, Math.random() - 0.5, camForward.z * 4);
+	for (var i = 0; i < count; i++) {
+		var rV = Math.random() * 6 - 3;
+		var v3R = new THREE.Vector3(camForward.z * rV, Math.random() * 2 - 1, camForward.x * -rV);
+		var particle = new Node(camPos.x + v3.x + v3R.x,
+								2 + v3R.y,
+								camPos.z + v3.z + v3R.z,
+								new THREE.SpriteMaterial( {
+								color: Math.random() * 0x808080 + 0x808080,
+								fog: true,
+								map: controller.materialLowDef 
+								} ), controller);
+		/*
+		particle.position.set(camPos.x + v3.x + v3R.x,
+								2 + v3R.y,
+								camPos.z + v3.z + v3R.z);
+		particle.scale.set(0.1, 0.1, 1);
+		*/
+		this.particles[i] = particle;
+		scene.add(particle);
+	
+		//console.log(particle);
+	};
+}
+
+RelatedNodes.prototype.updateNodes = function()
+{
+	for (var i = 0; i < this.particles.length; i++) {
+		if(this.particles[i].selected)
+		{
+			this.particles[i].position.set(this.particles[i].position.x + (this.particles[i].targetPosition.x - this.particles[i].position.x) * 0.2,
+										 this.particles[i].position.y + (this.particles[i].targetPosition.y - this.particles[i].position.y) * 0.2,
+										 this.particles[i].position.z + (this.particles[i].targetPosition.z - this.particles[i].position.z) * 0.2);
+			
+			this.particles[i].material.map = controller.materialHiDef;
+			this.particles[i].scale.x = this.particles[i].scale.y = this.particles[i].originalScale;
+		} else {
+			this.particles[i].position.set(this.particles[i].position.x + (this.particles[i].nodePosition.x - this.particles[i].position.x) * 0.02,
+											 this.particles[i].position.y + ((this.particles[i].nodePosition.y + Math.sin(Date.now()*0.001 + this.particles[i].position.x) * 0.02) - this.particles[i].position.y) * 0.06,
+											 this.particles[i].position.z + (this.particles[i].nodePosition.z - this.particles[i].position.z) * 0.02);
+				
+			this.particles[i].material.map = controller.materialBlurFar;
+			this.particles[i].scale.x = this.particles[i].scale.y = this.particles[i].originalScale * 1.45;
+		}
+	};
+}
+
+RelatedNodes.prototype.resetNodes = function(camPos, camForward)
+{
+	var v3 = new THREE.Vector3(camForward.x * 4, Math.random() - 0.5, camForward.z * 4);
+	for (var i = 0; i < this.particles.length; i++) {
+		var rV = Math.random() * 6 - 3;
+		var v3R = new THREE.Vector3(camForward.z * rV, Math.random() * 2 - 1, camForward.x * -rV);
+		
+		if(!this.particles[i].selected)
+			this.particles[i].position.set(camPos.x + v3.x + v3R.x,
+									(-this.particles[i].position.length() - 2) * 2,
+									camPos.z + v3.z + v3R.z);
+		
+		this.particles[i].nodePosition.set(camPos.x + v3.x + v3R.x,
+								2 + v3R.y,
+								camPos.z + v3.z + v3R.z);
+	};
 }
 
 Node.prototype = new THREE.Sprite;
 function Node(x, y, z, material, controller)
 {
 	THREE.Sprite.call(this, material);
-	this.scale.x = this.scale.y = Math.random() * 0.05 + 0.1;
-
+	
+	this.selected = false;
+	this.asleep = true;
+	this.scale.x = this.scale.y = Math.random() * 0.09 + 0.06;
+	this.connections = [];
+	this.contentID = null;
+	
+	this.controller = controller;
 	this.nodePosition = new THREE.Vector3(x, y, z);
+	this.selNodePosition = new THREE.Vector3(x, y, z);
 	this.introPosition = new THREE.Vector3(Math.round(x), 1.8, Math.round(z) + this.scale.x * 0.1);
+	this.targetPosition = new THREE.Vector3();
+	this.originalScale = this.scale.x;
+	this.position.x = this.introPosition.x;
+	this.position.y = (-this.position.length() - 2) * 2;
+	this.position.z = this.introPosition.z;
 
-	this.position.x = x;
-	this.position.y = 0;
-	this.position.z = z;
+	var v2Pos = new THREE.Vector2(this.position.x, this.position.z); 
 
+	if(v2Pos.length() > 12)
+	{
+		this.scale.x = this.scale.y = this.originalScale * 2.2;
+		this.material.map = controller.materialBlurFar;
+	}
+	else if(v2Pos.length() > 5)
+	{
+		this.scale.x = this.scale.y = this.originalScale * 1.5;
+		this.material.map = controller.materialBlur;
+	}
+	else
+	{
+		this.scale.x = this.scale.y = this.originalScale;
+		this.material.map = controller.materialLowDef;
+	}
+
+	var t = this;
+	setTimeout(function() { t.asleep = false }, (this.position.length() - 3) * 150);
+}
+
+Node.prototype.selectNode = function(target)
+{
+	this.selected = true;
+	this.targetPosition.set(target.x, target.y, target.z);
+
+	this.material.map = this.controller.materialHiDef;
+}
+
+Node.prototype.addConnection = function(target)
+{
+	connections[connections.length] = target;
+}
+
+Node.prototype.deselectNode = function()
+{
+	this.selected = false;
+
+	this.material.map = this.controller.materialLowDef;
+}
+
+Node.prototype.wakeUp = function()
+{
+	this.asleep = false;
 }
--- public/js/main.js
+++ public/js/main.js
@@ -8,32 +8,39 @@
 
 	// custom global variables
 	var dirV = new THREE.Vector3();
-	var W = 20, H = 20, SCL = 2;
+	var W = 20, H = 20, SCL = 2, sW = 2.5, sH = 2.5;
 
 	var camVel = new THREE.Vector2(), camPos = new THREE.Vector2();
 	var mousePressed = false;
 	var lon = 90, lat = 0;
 	var phi = 0, theta = 0;
 	var grid, controller = new NodeController();
-	// init();
+	var relatedNodes = null;
+	var nodeSelected = null;
+	var isInit = false;
+
 	animate();
 
-	// FUNCTIONS 		
+	// FUNCTIONS
 	function init() 
 	{
 		//Hide loader
-		document.getElementById('loader').style.display = "none";
-		
+		setTimeout(function () {
+			document.getElementById('loader').style.display = "none";
+		}, 3000);
+
 		// SCENE
 		scene = new THREE.Scene();
 		scene.fog = new THREE.Fog(0x000000, 5, 15);
 
 		// CAMERA
-		var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+		var SCREEN_WIDTH = window.innerWidth - 16, SCREEN_HEIGHT = window.innerHeight - 16;
 		var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 15;
 		camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
 		camera.position.y = 2;
 		scene.add(camera);
+		
+		isInit = true;
 
 		grid = createGrid();
 
@@ -43,18 +50,23 @@
 		container = document.getElementById( 'ThreeJS' );
 		container.appendChild( renderer.domElement );
 
-		var axes = new THREE.AxisHelper(100);
-		scene.add( axes );
-
 		document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 		window.addEventListener( 'mousemove', onMouseMove, false );
+		window.addEventListener( 'resize', onWindowResize, false );
+	
 	}
 	
 	function animate() {
+	    
 	    requestAnimationFrame( animate );
+		
+		if(!isInit)
+			return;
+		
 		render();
 		update();
 	}
+	
 	function update() {
 		camera.rotation.y += camVel.x;
 		camera.position.x += dirV.x * camVel.y;
@@ -63,37 +75,33 @@
 		camPos.y = Math.round(camera.position.z) / SCL;
 
 		updateGridNodes(grid);
+		if(relatedNodes != null)
+			relatedNodes.updateNodes();
 	}
 
 	function render() {
+
 		camera.getWorldDirection( dirV );
-		//updateInput();		
+
 		if(!mousePressed)
 		{
-			camVel.x *= 0.9;
-			camVel.y *= 0.9;
+			if(nodeSelected == null &&  controller.state != "intro")
+			{
+				camVel.x *= 0.9;
+				camVel.y *= 0.9;
+			}
+			else 
+			{
+				camVel.x = 0;
+				camVel.y = 0;
+			}
 		}
-
-		// update the picking ray with the camera and mouse position	
-		//raycaster.setFromCamera( mouse, camera );	
-
-		/*
-		// calculate objects intersecting the picking ray
-		var intersects = raycaster.intersectObjects( scene.children);
-
-		for ( var i = 0; i < intersects.length; i++ ) {
-
-			//intersects[ i ].object.material.color.set( 0x00ff00 );
 		
-		}
-		*/
 
 		if(keyboard.pressed("R"))
 		{
 			if(controller.state == "intro")
 				controller.state = "idle";
-			/*else
-				controller.state = "intro";*/
 		}
 
 		if(keyboard.pressed("b")) {
@@ -109,8 +117,6 @@
 
 	function createGrid() {
 		var g = [];
-		var texture =  new THREE.TextureLoader().load( "images/node.png" );
-
 
 		for (var i = W - 1; i >= 0; i--) {
 			g[i] = [];
@@ -119,13 +125,11 @@
 
 		for (var x = 0; x < W; x++) {
 			for (var y = 0; y < H; y++) {
-				g[x][y] = new Cluster((-W / 2 + x)*SCL, (-H / 2 + y)*SCL, texture, scene, controller);
+				g[x][y] = new Cluster((-W / 2 + x)*SCL, (-H / 2 + y)*SCL, scene, controller, camera);
 				scene.add(g[x][y]);
 			};
 		};
-
-		console.log(g);
-		console.log(controller);
+		
 		return g;
 	}
 
@@ -143,9 +147,85 @@
 		event.preventDefault();
 		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+		if(controller.state == "intro")
+		{
+			if(document.getElementById('loader').style.display == "none")
+				controller.state = "idle";
+			
+			return;
+		}
+
+		if(camVel.x <= 0.01 && camVel.y <= 0.01)
+		{
+			raycaster.setFromCamera( mouse, camera );	
+			raycaster.far = 6.5;
+			
+			var intersects = raycaster.intersectObjects( scene.children);
+
+			if(intersects.length > 0 && intersects[ 0 ].object.constructor.name == 'Sprite' && document.getElementById('big-modal').style.display != "block")
+			{
+				if(nodeSelected != null)
+				{
+					var b = false;
+					if(intersects[ 0 ].object.selected)
+						b = true;
+
+					nodeSelected.deselectNode();
+
+					if(b)
+					{
+						controller.state = "idle";
+        				big_modal.style.display = "none";
+						return;
+					}
+				}	
+				
+				controller.state = "selected";
+				camera.getWorldDirection( dirV );
+				
+				if(relatedNodes == null)
+					relatedNodes = new RelatedNodes(5, camera.position, dirV, controller);
+				else relatedNodes.resetNodes(camera.position, dirV);
+
+				nodeSelected = intersects[ 0 ].object;
+				nodeSelected.selectNode(camera.position.clone().add(dirV.clone().multiplyScalar(0.5)));
+				var url = "";
+
+				if(nodeSelected.contentID == null)
+					url = 'http://45.55.86.102:3003/api/post';
+				else url = 'http://45.55.86.102:3003/api/post/' + nodeSelected.contentID;
+				
+				getJSON(url, function(err, data){
+					if(err != null)
+				    {
+				    	console.log(err);
+				    	return;
+				    }
+					
+					showSmallModal(data);
+				});  
+			}
+			else if(intersects.length > 0)
+				console.log(intersects[ 0 ].object.constructor.name);
+			else if(nodeSelected != null && document.getElementById('small-modal').style.display != "block")
+			{	
+				if(nodeSelected != null)
+					nodeSelected.deselectNode();
+
+				controller.state = "idle";
+				nodeSelected = null;
+			}
+		}
 	}
 
 	function onDocumentMouseMove( event ) {
+		if(nodeSelected != null || controller.state == "intro" ||  document.getElementById('big-modal').style.display == "block")
+		{
+			camVel.set(0, 0);
+			return;
+		}
+
 		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
 		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 		camVel.x -= movementX * 0.00008;
@@ -158,12 +238,78 @@
 		document.removeEventListener( 'mouseup', onDocumentMouseUp );
 	}
 
-	function onMouseMove( event ) {
-		// calculate mouse position in normalized device coordinates
-		// (-1 to +1) for both components
-
+	function onMouseMove( event )
+	{
 		mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 	}
 
+	function onWindowResize(){
+
+	    camera.aspect = (window.innerWidth - 16) / (window.innerHeight - 16);
+	    camera.updateProjectionMatrix();
+
+	    renderer.setSize( window.innerWidth - 16, window.innerHeight - 16 );
+
+	}
+
+	function showSmallModal(data)
+	{
+		nodeSelected.contentID = data._id;
+
+		document.getElementById('small-modal').innerHTML = 
+					"<div class='small-modal'><div class='left-content pull-left'><div class='body-text'>" + data.content.title + "<br>" + 
+					"<hr /><a onclick='changeToBigModal()' >Ver más</a></div></div><div class='image pull-left'><img src='https://unsplash.it/160/120' /></div></div>";
+
+		document.getElementById('big-modal').innerHTML = 
+			"<div class='big-modal'><div class='row'><div class='col-xs-11'><div class='title'><h3>" + data.content.title + "</h3></div></div>" + 
+					"<div class='col-xs-1'><a onclick='closeBigModal()'><span class='glyphicon glyphicon-remove-circle close close-btn' aria-hidden='true'></span></a></div></div>" + 
+					"<hr /><div class='row'><div class='col-xs-6'><div class='image'><img src='https://unsplash.it/460/320' /></div></div><div class='col-xs-6'>" + data.content.text +
+					"<hr><div class='social-buttons'><img src='images/social.png' height='30px' /></div><hr><div class='tags'></div><small>Borges; infinito; universo;</small>" + 
+					"<hr><div class='relations'><img src='https://unsplash.it/80/60?random' /><img src='https://unsplash.it/80/60?random' /><img src='https://unsplash.it/80/60?random' /><img src='https://unsplash.it/80/60?random' /><img src='https://unsplash.it/80/60?random' />" + 
+					"</div></div></div><br><div class='source'><p>Fuente: TV Publica</p></div></div>";
+
+		document.getElementById('small-modal').style.display = "block";
+	}
+
+	function changeToBigModal()
+	{
+		document.getElementById('small-modal').style.display = "none";
+		document.getElementById('big-modal').style.display = "block";
+	}
+
+	function closeBigModal()
+	{
+		document.getElementById('big-modal').style.display = "none";
+	}
 	function de2ra(degree)   { return degree*(Math.PI/180); }
+	/*
+	function getJSONP(url, success) {
+	    var ud = '_' + +new Date,
+	        script = document.createElement('script'),
+	        head = document.getElementsByTagName('head')[0] 
+	               || document.documentElement;
+
+	    window[ud] = function(data) {
+	        head.removeChild(script);
+	        success && success(data);
+	    };
+
+	    script.src = url.replace('callback=?', 'callback=' + ud);
+	    head.appendChild(script);
+	}
+	*/
+	var getJSON = function(url, callback) {
+	    var xhr = new XMLHttpRequest();
+	    xhr.open("get", url, true);
+	    xhr.responseType = "json";
+	    xhr.onload = function() {
+	      var status = xhr.status;
+	      if (status == 200) {
+	        callback(null, xhr.response);
+	      } else {
+	        callback(status);
+	      }
+	    };
+	    xhr.send();
+	};
